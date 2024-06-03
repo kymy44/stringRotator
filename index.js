9b0b5b29eb115f710286b46560e6d0ea98db6e62
kymy44 <54514007+kymy44@users.noreply.github.com>
2024-06-03 11:40:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ let text = inicial.trim() + " ";
 
 function rotateString(string) {
 	array = string.split("");
-
 	removed = array.splice(-1);
-
 	return removed.concat(array).join("");
 }
 
 function rotateText() {
 	text = rotateString(text);
 	document.getElementById("cartel").innerHTML = text;
+	array = text.split("");
+	console.log(array[array.length - 1]);
 }
 
 setInterval(rotateText, 500);
